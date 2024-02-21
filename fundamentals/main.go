51bbad600a4179f05f0cb9ab413e38ea3ec0459b
todor1 <todorowa_el@abv.bg>
2024-02-21 16:15:33 +0200

    ch3 variables, pointers end

---
+++ fundamentals/main.go
@@ -0,0 +1,93 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello, World!")
+// 	println("Hello, Gophers!")
+// }
+
+// func main() {
+// 	// variables
+// 	var a string = "initial"
+// 	fmt.Println(a)
+
+// 	var b int = 1
+// 	fmt.Println(b)
+
+// 	var c bool = true
+// 	fmt.Println(c)
+
+// 	d := 3.1415
+// 	fmt.Println(d)
+
+// 	var e int = int(d)
+// 	fmt.Println(e)
+
+// 	// var f int8 = 54
+// 	// var g int16 = f
+// 	// var purvataMiPromenliva string = "Pesho"
+// 	// fmt.Println(purvataMiPromenliva)
+
+// }
+
+// func main() {
+// 	// operators
+// 	a, b := 5, 2
+// 	// arithmetic
+// 	println(a + b)
+// 	println(a - b)
+// 	println(a * b)
+// 	println(a / b)
+// 	println(a % b)
+
+// 	println(float32(a) / float32(b))
+// 	println(a == b)
+// 	println(a != b)
+// 	println(a < b)
+// 	println(a > b)
+// 	println(a <= b)
+// }
+
+// func main() {
+// 	// constants
+// 	// const a = 42
+// 	// var i int = a
+// 	// var f float64 = a
+// 	// fmt.Println(i, f)
+
+// 	// // const b float32 = 7.7474747  // error convertin to int
+// 	// const b float32 = 7
+// 	// var j int = int(b)
+// 	// var f32 float32 = b
+// 	// var f64 float64 = float64(b)
+// 	// fmt.Println(j, f32, f64)
+
+// 	// iota
+// 	const c = iota
+// 	fmt.Println(c)
+
+// 	const (
+// 		d = 2 * 5
+// 	)
+// 	fmt.Println(d)
+// }
+
+func main() {
+	// pointers
+	s := "Hello, World!"
+	fmt.Println(s)
+	p := &s
+	fmt.Println(p)
+	fmt.Println(*p)
+
+	*p = "Hello, Gophers!"
+	fmt.Println(s)
+
+	// p = "New value" // error
+	// fmt.Println(p)
+
+	p = new(string) // memory address change
+	fmt.Println(p, *p)
+
+}
