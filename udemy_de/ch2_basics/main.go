4ebd2a4b2696e36de45cda1da6999694a0450ab9
todor1 <todorowa_el@abv.bg>
2024-03-07 19:03:46 +0200

    udemy de ch2"

--- udemy_de/ch2_basics/main.go
+++ udemy_de/ch2_basics/main.go
@@ -1,7 +1,27 @@
 package main
 
-var x int
+import "fmt"
+
+// var x int
+var x = 32
 
 func main() {
-	println(x)
+	// println(x)
+	// fmt.Println(x)
+	// fmt.Printf("x is of type %T\n", x)
+	y := 42.4
+	// fmt.Println(y)
+	// fmt.Printf("y is of type %T\n", y)
+	z := "doh"
+	// fmt.Println(z)
+	// fmt.Printf("z is of type %T\n", z)
+
+	// outputThat(42, 42.4, "doh")
+	dpne, trueOrNot := outputThat(x, y, z)
+	fmt.Println(dpne, trueOrNot)
+}
+
+func outputThat(a int, b float64, c string) (string, bool) {
+	fmt.Printf("Integer = %d, float = %f, string = %s\n", a, b, c)
+	return "ok", true
 }
