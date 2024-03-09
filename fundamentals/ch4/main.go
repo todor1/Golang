9804e8a12f0979147b4b94b26051990b09fd577e
todor1 <todorowa_el@abv.bg>
2024-03-09 14:47:46 +0200

    ch4 finished

--- fundamentals/ch4/main.go
+++ fundamentals/ch4/main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"net/http"
+	"os"
+)
 
+// // CLI APP that takes a string and prints it in all caps with an exclamation point
+// func main() {
+// 	fmt.Println("What would you like me to scream?")
+// 	in := bufio.NewReader(os.Stdin)
+// 	s, _ := in.ReadString('\n')
+// 	s = strings.TrimSpace(s)
+// 	s = strings.ToUpper(s)
+// 	fmt.Println(s + "!")
+// }
+
+// Web Service
 func main() {
-	// ...
-	fmt.Println("Welcome to Chapter 4")
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(":3000", nil)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	f, _ := os.Open("./menu.txt")
+	io.Copy(w, f)
 }
